0a5ce4584da7758230c9b9ffb1e494ce89ade532
CristhianAguilera <cristhianh016@gmail.com>
2025-04-27 13:03:41 -0500

    Primer commit: Proyecto base

---
+++ src/main/java/com/utp/sistema_comandas/SistemaComandasApplication.java
@@ -0,0 +1,13 @@
+package com.utp.sistema_comandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaComandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaComandasApplication.class, args);
+	}
+
+}
