6b28c8895d75cc015511daf25986db1b4b25a4de
CristhianAguilera <cristhianh016@gmail.com>
2025-05-29 11:42:10 -0500

    chore: configurar commitlint con prefijo Jira opcional

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      // Prefijo tipo LPS-123 es opcional
+      headerPattern: /^(?:([A-Z]+-\d+)\s)?(feat|fix|chore|docs|style|refactor|test|ci)(\([a-zA-Z0-9\-]+\))?:\s.+$/,
+      headerCorrespondence: ["jira", "type", "scope", "subject"]
+    }
+  },
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "chore", "docs", "style", "refactor", "test", "ci"]
+    ],
+    "header-max-length": [2, "always", 100]
+  }
 };
