b8b57f9a32954e050ee8a81bb036e585e18f5c54
jdiego2005 <jdiegosg2005@gmail.com>
2025-05-30 22:05:10 -0500

    LPS-39 feat: Agregar funcionalidad para editar mozos

--- src/main/java/com/utp/sistema_comandas/Controllers/MozosController.java
+++ src/main/java/com/utp/sistema_comandas/Controllers/MozosController.java
@@ -1,6 +1,9 @@
 package com.utp.sistema_comandas.Controllers;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -55,6 +58,64 @@ public class MozosController {
             return ResponseEntity.badRequest().body(Map.of("error", "Error al registrar mozo"));
         }
 
+    }
+    @GetMapping("/admin/dotMozos")
+    public String dotMozos(HttpSession session, Model model) {
+        List<Usuario> usuarios = usuarioService.obtenerLosUsuarios();
+        model.addAttribute("usuarios", usuarios);
+
+        Usuario usuario = (Usuario) session.getAttribute("usuario");
+        model.addAttribute("usuario", usuario);
+        return "admin/dotMozos";
+    }
+
+    @GetMapping("/editarusuarioModal/{usuarioId}")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> obtenerUsuarioporId(@PathVariable Long usuarioId, Model model) {
+        // con el id del trabajdor buscamos su permiso ligado a el
+        Optional<Usuario> usuarioOpt = usuarioService.obtenerPorId(usuarioId);
+
+        // verificamos que permiso no sea nulo y que tenga el estado de Pendiente
+        if (usuarioOpt.isPresent()) {
+            Usuario usuario = usuarioOpt.get(); // Extraemos el objeto Usuario
+
+            Map<String, Object> usuarioData = new HashMap<>();
+            usuarioData.put("id", usuario.getId()); // también es clave para edición
+            usuarioData.put("nombre", usuario.getNombre());
+            usuarioData.put("apellido", usuario.getApellido());
+            usuarioData.put("correo", usuario.getCorreo());
+            usuarioData.put("dni", usuario.getDni());
+            usuarioData.put("telefono", usuario.getTelefono());
+            usuarioData.put("estado", usuario.getEstado());
+
+            return ResponseEntity.ok(usuarioData);
+        } else {
+            // retorna un error si no se enuentra el permiso
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "No se encontró este usuario."));
+        }
+    }
+
+    @PostMapping("/EditarMozo")
+    @ResponseBody
+    public ResponseEntity<?> editarMozo(@ModelAttribute Usuario usuario) {
+        try {
+            Optional<Usuario> usuarioOpt = usuarioService.obtenerPorId(usuario.getId());
+            if (usuarioOpt.isPresent()) {
+                Usuario usuarioAct = usuarioOpt.get();
+
+                usuarioAct.setNombre(usuario.getNombre());
+                usuarioAct.setApellido(usuario.getApellido());
+                usuarioAct.setTelefono(usuario.getTelefono());
+                usuarioAct.setEstado(usuario.getEstado());
+
+                usuarioService.guardUsuario(usuarioAct);
+                return ResponseEntity.ok().body(Map.of("success", true));
+            } else {
+                return ResponseEntity.badRequest().body(Map.of("error", "Usuario no encontrado"));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(Map.of("error", "Error al editar el mozo"));
+        }
     }
     
 }
